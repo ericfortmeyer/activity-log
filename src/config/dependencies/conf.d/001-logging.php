9ae1aab272f1fe8ffe22612bdcae5e3e2ee09e9e
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2025-12-19 23:04:24 -0500

    Use-exception-handling (#57)
    * refactor: use new exception handling feature
    
    Signed-off-by: Eric Fortmeyer <e.fortmeyer01@gmail.com>
    
    * build: upgrade to phpolar 8.3.0
    
    The project will now use the new exception handling feature.
    
    Signed-off-by: Eric Fortmeyer <e.fortmeyer01@gmail.com>

--- src/config/dependencies/conf.d/001-logging.php
+++ src/config/dependencies/conf.d/001-logging.php
@@ -9,10 +9,13 @@ declare(strict_types=1);
 namespace EricFortmeyer\ActivityLog;
 
 use EricFortmeyer\ActivityLog\DI\ServiceProvider;
+use Monolog\Formatter\LineFormatter;
 use Monolog\Handler\SyslogHandler;
 use Monolog\Logger;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
+
+use const EricFortmeyer\ActivityLog\DI\Tokens\EXCEPTION_LOGGER;
 
 return [
     LoggerInterface::class => static fn(ContainerInterface $container) =>
@@ -23,4 +26,14 @@ return [
             new ServiceProvider($container)->appConfig->appName
         ),
     ),
+    EXCEPTION_LOGGER => static fn(ContainerInterface $container) =>
+    new Logger(
+        new ServiceProvider($container)->appConfig->appName
+    )->pushHandler(
+        new SyslogHandler(
+            new ServiceProvider($container)->appConfig->appName
+        )->setFormatter(
+            new LineFormatter("%channel%.%level_name%: %message% %context%")
+        )
+    )
 ];
